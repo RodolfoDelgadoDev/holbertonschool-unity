d92791b6bbbb5402e0cf78d98422b87f6a711cc9
Rodolfo Delgado <2786@holbertonschool.com>
2022-02-14 13:59:16 -0300

    added ex 2

--- 0x06-unity-assets_ui/Assets/Scripts/BottonChanger.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/BottonChanger.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// BottonChanger class.
+/// </summary>
 public class BottonChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public Sprite SpriteOnMouse;
-    public Sprite SpriteClick;
+    
 
     void Start()
     {
