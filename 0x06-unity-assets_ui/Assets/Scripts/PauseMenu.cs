b625b61cb458746bde81537e3579e8c4a420339d
Rodolfo Delgado <2786@holbertonschool.com>
2022-02-15 11:44:52 -0300

    fixing errors from ex 1 (align text to the center and middle)

---
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// PauseMenu class to manage the Pasue menu
+/// </summary>
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
